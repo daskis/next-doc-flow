02fba5cf6e98a6cda25b3dd7a559933d1afa6415
Anton Zhuravlev <abramoff.job@gmail.com>
2023-04-23 11:23:19 +0300

    123

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import {Layout} from "@/Layout/Layout";
+import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-  )
+export default function App({Component, pageProps}: AppProps) {
+    if (!process.browser) React.useLayoutEffect = React.useEffect;
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    )
 }
